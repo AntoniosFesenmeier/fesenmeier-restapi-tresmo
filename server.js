7448ab0f08edb5a6dd79fc3c4416257b7a036d4b
AntoniosFesenmeier <fesetoni@gmail.com>
2016-10-10 18:01:57 +0200

    charset test / portz

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ server.post('api/v1/wines', wines.createWine);
 server.put('api/v1/wines/:id', wines.editWine);
 server.del('api/v1/wines/:id', wines.delWine);
 
-var port = process.env.TRESMO_PORT;
+var port = process.env.PORT || process.env.TRESMO_PORT;
 server.listen(port, function () {
     console.log('Server started at Port: ' + port);
 });
